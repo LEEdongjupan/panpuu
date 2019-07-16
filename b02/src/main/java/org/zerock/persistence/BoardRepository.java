d376cb6d0ca9b30680eb0131fc3889e331465288
djlee <panpuu27@gmail.com>
2019-07-16 09:10:40 +0900

    spring boot first time

---
+++ b02/src/main/java/org/zerock/persistence/BoardRepository.java
@@ -0,0 +1,28 @@
+package org.zerock.persistence;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.zerock.domain.Board;
+
+public interface BoardRepository extends CrudRepository<Board, Long> {
+
+	public List<Board> findByTitle(String title);
+
+//	  public Collection<Board> findByWriter(String writer);
+	
+	 public Collection<Board> findByWriterContaining(String writer);
+	 
+	 public List<Board> findByBnoGreaterThanOrderByBnoDesc(Long bno, Pageable paging);
+	 
+	 public Page<Board> findByBnoGreaterThan(Long bno, Pageable paging);
+
+
+	 @Query("SELECT b FROM Board b WHERE b.title LIKE %?1% AND b.bno > 0 ORDER BY b.bno DESC")
+	  public List<Board> findByTitleQuery(String title);
+
+}
---
+++ b02/src/test/java/org/zerock/BoardRepositoryTests.java
@@ -0,0 +1,134 @@
+package org.zerock;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.zerock.domain.Board;
+import org.zerock.persistence.BoardRepository;
+
+import lombok.Setter;
+import lombok.extern.java.Log;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Log
+public class BoardRepositoryTests {
+
+	  @Setter(onMethod_= {@Autowired} )
+	  private BoardRepository boardRepo;
+
+	  @Test
+	  public void testByTitle2Query( ){
+	    
+	    boardRepo.findByTitleQuery("17")
+	    .forEach(board -> System.out.println(board));
+	    
+	  }
+
+	  @Test
+	  public void testBnoOrderByPaging() {
+
+	    //Spring Boot 1.x
+	    //Pageable paging = new PageRequest(0, 10);
+	    //Spring Boot 2.x
+	    Pageable paging = PageRequest.of(0, 10);
+
+	    Collection<Board> results = boardRepo.findByBnoGreaterThanOrderByBnoDesc(0L, paging);
+	    results.forEach(board -> System.out.println(board));
+
+	  }
+
+	  @Test
+	  public void testBnoPagingSort() {
+
+	    Pageable paging = PageRequest.of(0, 10, Sort.Direction.ASC, "bno");
+
+	    Page<Board> result = boardRepo.findByBnoGreaterThan(0L, paging);
+	    
+	    System.out.println("PAGE SIZE: " + result.getSize());
+	    System.out.println("TOTAL PAGES: " + result.getTotalPages());
+	    System.out.println("TOTAL COUNT: " + result.getTotalElements());
+	    System.out.println("NEXT: " + result.nextPageable());
+	    
+	    List<Board> list = result.getContent();
+	    
+	    list.forEach(board -> System.out.println(board));
+	    
+	  }
+
+	  
+	  @Test
+	  public void testByWriter() {
+
+	    Collection<Board> results = boardRepo.findByWriterContaining("10");
+
+	    results.forEach(
+	        board -> System.out.println(board)
+	    );
+	  }
+
+
+	  
+	  
+	  @Test
+	  public void testInsert() {
+
+		  IntStream.range(0, 100).forEach((i)->{
+			  Board board = new Board();
+			    board.setTitle("게시물의 제목"+i);
+			    board.setContent("게시물 내용 넣기...."+i);
+			    board.setWriter("user"+i);
+
+			    boardRepo.save(board);
+		  });
+	  }
+		  @Test
+		  public void testRead(){
+		    
+		    //해당 게시물 번호가 있는지 우선 확인하고 진행할 것 
+		    boardRepo.findById(1L)
+		            .ifPresent(board -> System.out.println(""+board));
+		    
+		  }
+	  
+		  
+	    
+
+		  @Test
+		  public void testUpdate(){
+		    
+		   log.info("Read First.........................");
+		    Board board = boardRepo.findById(1L).get();
+		    
+		    log.info("Update Title.......................");
+		    board.setTitle("수정된 제목입니다");
+		    
+		    log.info("Call Save( ).......................");
+		    boardRepo.save(board);
+		    
+		  }
+
+
+		  @Test
+		  public void testDelete(){
+		    
+		    System.out.println("DELETE Entity ");
+		    
+		    //1.5 
+		    //boardRepo.delete(1L);
+		    //2.x
+		    boardRepo.deleteById(1L);
+		  }
+
+	
+}
