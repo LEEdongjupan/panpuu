11f0801910a73abae36bf9c3daf527655be05ad7
djlee <panpuu27@gmail.com>
2019-07-22 10:15:04 +0900

    springboot web first

---
+++ b04/src/main/java/org/zerock/B04Application.java
@@ -0,0 +1,13 @@
+package org.zerock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B04Application.class, args);
+	}
+
+}
