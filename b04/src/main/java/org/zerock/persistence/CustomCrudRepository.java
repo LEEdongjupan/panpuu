49d07ef622591b92c2e441a7a886d5c05c7470af
djlee <panpuu27@gmail.com>
2019-07-23 12:48:22 +0900

    comment finish

---
+++ b04/src/main/java/org/zerock/persistence/CustomCrudRepository.java
@@ -0,0 +1,9 @@
+package org.zerock.persistence;
+ 
+import org.springframework.data.repository.CrudRepository;
+import org.zerock.domain.WebBoard;
+ 
+public interface CustomCrudRepository extends CrudRepository<WebBoard, Long>, CustomWebBoard {
+	
+	
+}
